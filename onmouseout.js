b7c28055da63d785a4c011e75d5abce1fc1eafac
Bata-san <60231172+batasrdanovic@users.noreply.github.com>
2020-04-10 23:06:20 +0200

    Add files via upload

---
+++ onmouseout.js
@@ -0,0 +1,26 @@
+/* NAPOMENA: OVAJ ZADATAK NEMA U SKRIPTI VEC SAMO SE POMINJE KAO OPCIJA
+DA IMA FUNKCIJA ONMOUSEOUT  
+12.ZADATAK
+Reagovati na događaj onmouseout sa funkcijom po izboru.    */
+
+
+
+function nestani(){
+    document.write("Nece se nista pojaviti");
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
